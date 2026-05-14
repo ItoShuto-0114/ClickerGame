6d7250436a0c2da514abd256cb11e2b4236a3757
shuto20051014 <shuto20051014@gmail.com>
2026-05-14 17:53:21 +0900

    [update]ボールのバグ修正

--- ClickerGameeee/Assets/ball/ball.cs
+++ ClickerGameeee/Assets/ball/ball.cs
@@ -2,29 +2,38 @@ using UnityEngine;
 
 public class ball : MonoBehaviour
 {
+    private Vector2 _mousePressPos;
+    private Vector2 _mouseReleasePos;
+    private Vector2 _dir;
+    private bool _isDragging;
+    Rigidbody2D _rb;
     void Start()
     {
-        
+        _rb= GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
-
-            if (hit.collider != null)
-            {
-                Debug.Log("èRÇËÇ‚Ç™Ç¡ÇΩÇ»");
-                Kick();
-            }
+            _mousePressPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); ;
+            _isDragging = true;
+        }
+        if (Input.GetMouseButton(0) && _isDragging)
+        {
+            _mouseReleasePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if(Input.GetMouseButtonUp(0)&&_isDragging)
+        {
+            Kick();
+            _isDragging=false;
         }
     
     }
     void Kick()
     {
-        Destroy(gameObject);
+        _dir = _mousePressPos - _mouseReleasePos;
+        _rb.linearVelocity = Vector2.zero;
+        _rb.AddForce(_dir * 5f, ForceMode2D.Impulse);
     }
 }
