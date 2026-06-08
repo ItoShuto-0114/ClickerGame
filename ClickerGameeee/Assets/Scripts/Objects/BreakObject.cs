78f0b74d560884670e6de29cd0ae19f803873b10
shuto20051014 <shuto20051014@gmail.com>
2026-06-09 01:37:32 +0900

    [add]壊せるオブジェクト用のフォルダ作成

---
+++ ClickerGameeee/Assets/Scripts/Objects/BreakObject.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BreakObject : MonoBehaviour
+{
+    [SerializeField] ObjectType _type;
+    [SerializeField]private ObjectData _data;
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
