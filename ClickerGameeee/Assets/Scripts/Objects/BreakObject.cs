3fcdf164ab5e8b9576ace8217ec57fb49faf0d49
shuto20051014 <shuto20051014@gmail.com>
2026-06-10 02:25:33 +0900

    [add]破壊できるオブジェクトwoodの追加

--- ClickerGameeee/Assets/Scripts/Objects/BreakObject.cs
+++ ClickerGameeee/Assets/Scripts/Objects/BreakObject.cs
@@ -3,14 +3,31 @@ using UnityEngine;
 public class BreakObject : MonoBehaviour
 {
     [SerializeField] ObjectType _type;
-    [SerializeField]private ObjectData _data;
-    void Awake()
+    [SerializeField]private BreakObjectDataBase _objectDataBase;
+    [SerializeField] GameManager _gm;
+    private ObjectData _data;
+    int _hp;
+    int _reward;
+    
+    private void Start()
     {
-        
+        _data = _objectDataBase.GetupDataBase(_type);
+        _hp = _data.Hp;
+        _reward = _data.Reward;
     }
 
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        _hp -= damage;
+        if (_hp <= 0)
+        {
+            Broken();
+        }
+    }
+    public void Broken()
+    {
+        Destroy(gameObject);
+        _gm._money += _reward;
+
     }
 }
