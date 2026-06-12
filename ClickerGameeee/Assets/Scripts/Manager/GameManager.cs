586b126f0277e030f9ece315e59b539df396c5c1
shuto20051014 <shuto20051014@gmail.com>
2026-06-13 00:10:02 +0900

    [update]ObjectUpgradeボタンの追加

--- ClickerGameeee/Assets/Scripts/Manager/GameManager.cs
+++ ClickerGameeee/Assets/Scripts/Manager/GameManager.cs
@@ -7,15 +7,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] private PowerUpgradeDetaBase _powerDataBase;
     [SerializeField] private MoneyUpgradeDataBase _moneyUpgradeDatabase;
     [SerializeField] private BreakObjectDataBase _objectData;
+    [SerializeField] private ObjectUpgradeDataBase _objectUpgradeDatabase;
     [SerializeField,Header("金UI")] TextMeshProUGUI _countUI; 
     [SerializeField,Header("PowerLvのUI")] TextMeshProUGUI _powerLevelUI;
     [SerializeField, Header("MoneyLvのUI")] TextMeshProUGUI _moneyLevelUI;
+    [SerializeField, Header("ObjectLvのUI")] TextMeshProUGUI _objectLevelUI;
     [SerializeField] UpGradeManager _ugm;
     [SerializeField] TextMeshProUGUI _powerUpButtonText;
     [SerializeField] TextMeshProUGUI _moneyUpButtonText;
+    [SerializeField] TextMeshProUGUI _objectUpButtonText;
     public int _money = 0;
     public int _powerCost = 0;
     public int _moneyCost = 0;
+    public int _objectLevelCost = 0;
     public int _kickCount;
     public static GameManager Instance { get; private set; }
 
@@ -31,18 +35,21 @@ public class GameManager : MonoBehaviour
         _powerDataBase.CreateDataBase();
         _moneyUpgradeDatabase.CreateDataBase();
         _objectData.CreateDataBase();
+        _objectUpgradeDatabase.CreateDataBase();
     }
     void Update()
     {
         PowerUpgradedeta powerdata = _powerDataBase.GetUpGradeData(_ugm.CurrentPowerLevel);
         MoneyUpgradedata moneydata = _moneyUpgradeDatabase.GetUpGradeData(_ugm.CurrentMoneyLevel);
-        if (powerdata == null || moneydata == null)
+        ObjectUpgradeData objectdata = _objectUpgradeDatabase.GetUpGradeData(_ugm.CurrentObjectLevel);
+        if (powerdata == null || moneydata == null || objectdata == null)
         {
             Debug.LogError("データねえよカス");
             return;
         }
         _powerCost = powerdata.Cost;
         _moneyCost = moneydata.Cost;
+        _objectLevelCost = objectdata.ObjectLevelCost;
      UpdateUI();
     }
     void UpdateUI()
--- ClickerGameeee/Assets/Scripts/Manager/UpGradeManager.cs
+++ ClickerGameeee/Assets/Scripts/Manager/UpGradeManager.cs
@@ -6,27 +6,38 @@ public class UpGradeManager : MonoBehaviour
     private int _currentPowerLevel = 1;
     public int CurrentMoneyLevel => _currentMoneyLevel;
     private int _currentMoneyLevel = 1;
-    [SerializeField] GameManager _gm;
-    
+    public int CurrentObjectLevel => _currentObjectLevel;
+    private int _currentObjectLevel = 1;
+
     public void PowerUpGrade()
     {
-        if (_gm._money < _gm._powerCost)
+        if (GameManager.Instance._money < GameManager.Instance._powerCost)
         {
             Debug.Log("レベルアップできねえよ");
             return;
         }
 
         _currentPowerLevel++;
-        _gm._money -= _gm._powerCost;
+        GameManager.Instance._money -= GameManager.Instance._powerCost;
     }
     public void MoneyUpGrade()
     {
-        if (_gm._money < _gm._moneyCost)
+        if (GameManager.Instance._money < GameManager.Instance._moneyCost)
         {
             Debug.Log("レベルアップできねえよ");
             return;
         }
         _currentMoneyLevel++;
-        _gm._money -= _gm._moneyCost;
+        GameManager.Instance._money -= GameManager.Instance._moneyCost;
+    }
+    public void ObjectLevelUpGrade()
+    {
+        if(GameManager.Instance._money < GameManager.Instance._objectLevelCost)
+        {
+            Debug.Log("レベルアップできねえよ");
+            return;
+        }
+        _currentObjectLevel++;
+        GameManager.Instance._money -= GameManager.Instance._objectLevelCost;
     }
 }
